b47b678cd95e01ea2300fdf8391b8d9f0f083270
YoungMo Cheon <dudah8713@live.co.kr>
2022-02-24 02:09:24 +0900

    Tool FrameWork Init

---
+++ FrameWork/Tool_YM/Code/Tool_Defines.h
@@ -0,0 +1,10 @@
+#ifndef Tool_Defines_h__
+#define Tool_Defines_h__
+
+static const unsigned int g_iWINCX = 1280;
+static const unsigned int g_iWINCY = 720;
+
+extern HINSTANCE g_hInst;
+extern HWND g_hWnd;
+
+#endif // Tool_Defines_h__
