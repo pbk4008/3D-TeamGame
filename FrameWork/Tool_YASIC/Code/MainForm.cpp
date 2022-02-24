67ecb9c08fb3b63227055e5772c3ac5f3ba991e1
Y4SIC <48639322+Y4SIC@users.noreply.github.com>
2022-02-24 09:37:16 +0900

    툴 기초 작업

---
+++ FrameWork/Tool_YASIC/Code/MainForm.cpp
@@ -0,0 +1,49 @@
+﻿// MainForm.cpp: 구현 파일
+//
+
+#include "pch.h"
+#include "Tool_YASIC.h"
+#include "MainForm.h"
+
+
+// CMainForm
+
+IMPLEMENT_DYNCREATE(CMainForm, CFormView)
+
+CMainForm::CMainForm()
+	: CFormView(IDD_CMainForm)
+{
+
+}
+
+CMainForm::~CMainForm()
+{
+}
+
+void CMainForm::DoDataExchange(CDataExchange* pDX)
+{
+	CFormView::DoDataExchange(pDX);
+}
+
+BEGIN_MESSAGE_MAP(CMainForm, CFormView)
+END_MESSAGE_MAP()
+
+
+// CMainForm 진단
+
+#ifdef _DEBUG
+void CMainForm::AssertValid() const
+{
+	CFormView::AssertValid();
+}
+
+#ifndef _WIN32_WCE
+void CMainForm::Dump(CDumpContext& dc) const
+{
+	CFormView::Dump(dc);
+}
+#endif
+#endif //_DEBUG
+
+
+// CMainForm 메시지 처리기
