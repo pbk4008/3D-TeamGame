8f3d03048bdaa1635283fdf0f95129b1b454b4d3
Alen <chaicofski@gmail.com>
2022-02-24 17:18:34 +0900

    All Tool

--- FrameWork/ShaderExamples/default/Defines.h
+++ FrameWork/ShaderExamples/default/Defines.h
@@ -6,7 +6,7 @@ static const unsigned long	g_iWinCX = 1280;
 static const unsigned long	g_iWinCY = 720;
 static const float			g_fCamFar = 900.f;
 
-enum LEVEL { LEVEL_STATIC, LEVEL_LOADING, LEVEL_LOGO, LEVEL_GAMEPLAY, LEVEL_END };
+enum  class LEVEL { LEVEL_STATIC, LEVEL_LOADING, LEVEL_LOGO, LEVEL_GAMEPLAY, LEVEL_END };
 
 #include "Engine_Defines.h"
 
