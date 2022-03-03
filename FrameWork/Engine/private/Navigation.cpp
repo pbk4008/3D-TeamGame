11fc0b9b9147ba7c5edc9c601f5ca791b2f0c387
YoungMo Cheon <dudah8713@live.co.kr>
2022-03-03 23:30:51 +0900

    Revert "YM commit"
    This reverts commit 68a2696ac938a8f646816c2633f7c8df1201d385.

--- FrameWork/Engine/private/Navigation.cpp
+++ FrameWork/Engine/private/Navigation.cpp
@@ -137,12 +137,21 @@ HRESULT CNavigation::Update_Buffer(_fvector pPosition)
 				Finder->m_vPoint[i] = *(Finder->m_pPoint[i]);
 				pCell = Finder;
 			}
+
 			if (nullptr != pCell)
 			{
 				D3D11_MAPPED_SUBRESOURCE resource;
-				m_pDeviceContext->Map(pCell->m_pVIBuffer->m_pVB, 0, D3D11_MAP_WRITE_NO_OVERWRITE, 0, &resource);
+				m_pDeviceContext->Map(pCell->m_pVIBuffer->m_pVB, 0,  D3D11_MAP_WRITE_DISCARD, 0, &resource);
+
+				resource.pData = pCell->m_pVIBuffer->getVertices();
+
+				VTXCOL* Vtx = (VTXCOL*)resource.pData;
+
 				for (int i = 0 ; i < CCell::POINT_END; ++i)
-					((VTXCOL*)resource.pData)[i].vPosition = *pCell->m_pPoint[i];
+				{
+					Vtx[i].vPosition = *pCell->m_pPoint[i];
+				}
+				pCell->m_pVIBuffer->Create_VertexBuffer();
 				m_pDeviceContext->Unmap((pCell)->m_pVIBuffer->m_pVB, 0);
 			}
 		} 
